0f2f1e03916ad5126a99e8234143b129f7099def
Virtual Jasper <jasper2virtual@hotmail.com>
2024-11-11 18:25:20 +0800

    fix: update App.vue to use min-height for desktop layout

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen min-w-[320px] p-4 flex flex-col bg-app-neutral-veryDarkBlue justify-center items-center">
     <main class="grow px-4 py-6 container flex justify-center items-center">
       <div class="flex flex-col items-center gap-8
-    desktop:h-[50vh] desktop:grid desktop:grid-cols-4 desktop:grid-rows-2">
+    desktop:min-h-[50vh] desktop:grid desktop:grid-cols-4 desktop:grid-rows-2">
         <ReportControl name="Jeremy Robson" @showDaily="showDaily" @showWeekly="showWeekly" @showMonthly="showMonthly"
           class="w-full max-w-96
       desktop:row-span-2 desktop:self-stretch
